8113bfc0fd8767d3dc5d590710fc7dde7cbeadd1
inperium <ibow9416@740s-pl-111.local>
2017-04-10 13:34:24 -0600

    Created the inheritance project and added an implemented method

---
+++ src/model/SpecialMove.java
@@ -0,0 +1,12 @@
+package model;
+
+//@author arick.smith
+//@author isaac.bowen
+
+public interface SpecialMove 
+{
+	public int hitDamage();
+	public String someName(String name);
+	public boolean wreckable();
+	public int defence();
+}
