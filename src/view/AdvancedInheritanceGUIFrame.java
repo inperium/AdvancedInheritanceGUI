f9b4b2e319f62316c8112d9612f2ba156db5e576
inperium <ibow9416@740s-pl-111.local>
2017-04-10 13:53:23 -0600

    Created the frame

--- src/view/AdvancedInheritanceGUIFrame.java
+++ src/view/AdvancedInheritanceGUIFrame.java
@@ -1,5 +1,38 @@
 package view;
 
-public class AdvancedInheritanceGUIFrame {
+import java.awt.Dimension;
+import javax.swing.JFrame;
 
+
+import controller.AdvancedInheritanceGUIController;
+
+public class AdvancedInheritanceGUIFrame extends JFrame
+{
+	private AdvancedInheritanceGUIController baseController;
+	private AdvancedInheritanceGUIPanel basePanel;
+	
+	public AdvancedInheritanceGUIFrame(AdvancedInheritanceGUIController baseController)
+	{
+		super();
+		this.baseController = baseController;
+		basePanel = new AdvancedInheritanceGUIPanel(baseController);
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(basePanel);
+		this.setTitle("Inheritance");
+		this.setSize(new Dimension(1080, 600));
+		this.setResizable(false);
+		this.setVisible(true);
+	}
+	public AdvancedInheritanceGUIController getBaseController()
+	{
+		return baseController;
+	}
+	public void setBaseController(AdvancedInheritanceGUIController baseController)
+	{
+		this.baseController = baseController;
+	}
 }
