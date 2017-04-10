0906e74b344b18bc7a04c18f119d0d0b237ec4e8
inperium <ibow9416@740s-pl-111.local>
2017-04-10 13:51:14 -0600

    Added the panel to the inheritance gui

--- src/view/AdvancedInheritanceGUIPanel.java
+++ src/view/AdvancedInheritanceGUIPanel.java
@@ -1,5 +1,64 @@
+
 package view;
 
-public class AdvancedInheritanceGUIPanel {
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+import controller.AdvancedInheritanceGUIController;
 
+import java.awt.Color;
+
+import javax.swing.*;
+
+
+public class AdvancedInheritanceGUIPanel extends JPanel
+{
+	private AdvancedInheritanceGUIController baseController;
+	private SpringLayout baseLayout;
+	private JButton jacobsButton;
+	private JButton issacsButton;
+	private JButton tylersButton;
+	private JButton aricksButton;
+	
+	public AdvancedInheritanceGUIPanel(AdvancedInheritanceGUIController baseController)
+	{
+		this.baseController = baseController;
+		baseLayout = new SpringLayout();
+		jacobsButton = new JButton("Jacob's button");
+		issacsButton = new JButton("Issac's button");
+		tylersButton = new JButton("Tyler's button");
+		aricksButton = new JButton("Arick's button");
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout);
+		this.setBackground(Color.GRAY);
+		this.add(jacobsButton);
+		this.add(issacsButton);
+		this.add(tylersButton);
+		this.add(aricksButton);
+		
+	}
+	
+	private void setupLayout()
+	{
+		baseLayout.putConstraint(SpringLayout.WEST, jacobsButton, 10, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, jacobsButton, -10, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, issacsButton, 10, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, issacsButton, -10, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, tylersButton, 0, SpringLayout.NORTH, jacobsButton);
+		baseLayout.putConstraint(SpringLayout.EAST, tylersButton, 0, SpringLayout.EAST, issacsButton);
+		baseLayout.putConstraint(SpringLayout.NORTH, aricksButton, 0, SpringLayout.NORTH, issacsButton);
+		baseLayout.putConstraint(SpringLayout.WEST, aricksButton, 0, SpringLayout.WEST, jacobsButton);
+	}
+	
+	private void setupListeners()
+	{
+		
+		
+	}
 }
